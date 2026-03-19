02b179a87851eef2b457b334d69bee8f86cb624e
CodeBySrinivasan <srinimuthu80@gmail.com>
2026-03-19 17:33:47 +0530

    Update FastAPI server configuration
    Change server host to '0.0.0.0' and port to 7860.

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ async def remove_background(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     # Standard FastAPI port
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
